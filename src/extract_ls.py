aab03b5b7acd4450fa45ccfdccfe8eb3020bc954
LPorcaro <lorenzo.porcaro@gmail.com>
2022-06-29 16:57:12 +0200

    Add LS data

--- src/extract_ls.py
+++ src/extract_ls.py
@@ -4,16 +4,31 @@
 import os
 import pandas as pd
 import zipfile
+import csv
 
-LS_FOLDER = "/home/lorenzo/Downloads/longterm/psytoolkit/LS"
+LS_FOLDER = "/home/lorenzoporcaro/Downloads/longterm/LS"
 LS_OUT = "../data/ls"
 
 
 if __name__ == "__main__":
+
+    HD = []
+    with open("../data/HD_PID.csv", 'r') as inf:
+        _reader = csv.reader(inf)
+        for row in _reader:
+            HD.append(row[0])
+    LD = []
+    with open("../data/LD_PID.csv", 'r') as inf:
+        _reader = csv.reader(inf)
+        for row in _reader:
+            LD.append(row[0])
+
     
-    for infile in os.listdir(LS_FOLDER):
+    for infile in sorted(os.listdir(LS_FOLDER)):
         print("Extracting: {}".format(infile))
         outfile = infile.replace(".zip", ".csv")
+        outfile = outfile.replace("G1", "HD")
+        outfile = outfile.replace("G2", "LD")
         infile = os.path.join(LS_FOLDER, infile)
         zf = zipfile.ZipFile(infile) 
         df = pd.read_excel(zf.open('data.xlsx'))
@@ -27,10 +42,15 @@ if __name__ == "__main__":
         df_out = df_out.rename(columns=c_dict)
         df_out = df_out.sort_values(by="PROLIFIC_PID")
 
-        if "G1" in outfile:
-            outfile = os.path.join(LS_OUT, 'g1', outfile)
+
+        if "HD" in outfile:
+            if df['PROLIFIC_PID'].isnull().values.any():
+                print(set(HD).difference(set(df['PROLIFIC_PID'].tolist())))
+            outfile = os.path.join(LS_OUT, 'HD', outfile)
             df_out.to_csv(outfile, index=False)
-        elif "G2" in outfile:
-            outfile = os.path.join(LS_OUT, 'g2', outfile)
+        elif "LD" in outfile:
+            if df['PROLIFIC_PID'].isnull().values.any():
+                print(set(LD).difference(set(df['PROLIFIC_PID'].tolist())))
+            outfile = os.path.join(LS_OUT, 'LD', outfile)
             df_out.to_csv(outfile, index=False)
 
